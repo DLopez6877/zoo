0e7653d100bf462ac8837d929ffa116084690164
Daniel <dlopez6877@gmail.com>
2017-07-07 17:00:40 -0700

    add new animals

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,7 +19,10 @@ export class AppComponent {
   selectedAnimal = null;
   masterAnimalList: Animal[] = [
     new Animal('Arctic Fox', 'Moon', 2, 'Carnivore', 'Pacific Shores', 5, 'Female', 'Cool Shade', 'Loud Noises'),
-    new Animal('Ocelot', 'Prince', 4, 'Carnovore', 'Africa', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Ocelot', 'Prince', 4, 'Carnivore', 'Africa', 6, 'Male', 'Laying in the sunshine', 'Toys that are not rope-based'),
+    new Animal('Elephant', 'Junior', 4, 'Herbivore', 'Africa', 6, 'Male', 'Long walks on the beach', 'Kids'),
+    new Animal('Hippo', 'Purpurp', 4, 'Carnivore', 'Pacific Shores', 6, 'Female', 'Candy', 'Ice Cream Sandwhiches'),
+    new Animal('Polar Bear', 'John Stark', 4, 'Carnivore', 'Africa', 6, 'Male', 'Long Baths', 'Hot Sunny Days'),
     new Animal('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Great Northwest', 2, 'Female', 'Delicate roots and leaves', 'Loud Noises')
   ];
 
